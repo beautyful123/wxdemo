d1feeaa28afe91a5f9f86c37d92c54b2890408a4
yulw <yulw@anchnet.com>
2021-02-02 16:34:14 +0800

    拓展

--- pages/eat/index.js
+++ pages/eat/index.js
@@ -5,14 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    src: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let src = options.src
+    this.setData({src})
   },
 
   /**
