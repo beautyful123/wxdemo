d930b28ef167a1931eddce6831cf4d064b411337
yulw <yulw@anchnet.com>
2021-03-22 15:09:15 +0800

    测啊删除

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,8 +1,14 @@
 // custom-tab-bar/index.js
-Page({
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
 
   /**
-   * 页面的初始数据
+   * 组件的初始数据
    */
   data: {
     active: 0,
@@ -14,80 +20,48 @@ Page({
       '/pages/lsjl/lsju'
     ]
   },
-  onChange(event) {
-    // event.detail 的值为当前选中项的索引
-    let detail = event.detail;
-    let url = this.data.list[detail]
-    if(detail === 2) {
-      this.openScanCode()
-    } else {
-      wx.switchTab({
-        url
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    openScanCode() {
+      wx.scanCode({
+        success(res) {
+          console.log(res)
+        }
       })
-    };
-    this.setData({ active: detail });
-    console.log(this.data.active)
-    console.log(detail)
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-  openScanCode() {
-    wx.scanCode({
-      success(res) {
-        console.log(res)
-      }
-    })
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    console.log("于立伟真帅")
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    },
+    onChange(event) {
+      // event.detail 的值为当前选中项的索引
+      let detail = event.detail;
+      let url = this.data.list[detail]
+      this.setData({ active: detail },() => {
+        if(detail === 2) {
+          this.openScanCode()
+        } else {
+          wx.switchTab({
+            url
+          })
+        };
+      });
+      console.log(detail)
+      console.log(this.data.active)
+      setTimeout(()=> {
+        console.log(detail)
+        console.log(this.data.active)
+      },3000)
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      console.log(page)
+      const that = this
+         this.setData({
+        　  active: this.data.list.findIndex(item => item === `/${page.route}`)
+         }, () => {
+           console.log(`/${page.route}`)
+           console.log(that.data.active)
+         });
+    }
   }
 })
